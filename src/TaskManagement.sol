820c3de1b6f54078fd37ec46238d734f7fbbad6e
Franky <funfrank4591@gmail.com>
2024-11-25 17:42:34 +0530

    updates task data retrieval using arrays

--- src/TaskManagement.sol
+++ src/TaskManagement.sol
@@ -14,7 +14,10 @@ contract TaskManagement is BaseTask {
         uint stakeValue
     ) internal isCreatorRole(reward, stakeValue) {
         require(stakeValue >= reward, "reward must be deposited");
-
+        require(
+            stakeValue >= reward + CREATOR_STAKE,
+            "Reward and stake must be deposited, currently reward is  {$reward} stake {$CREATOOR_STAKE} "
+        );
         Task storage newTask = tasks[taskCounter];
         newTask.id = taskCounter;
         newTask.creator = msg.sender;
@@ -50,33 +53,42 @@ contract TaskManagement is BaseTask {
     // }
 
     // Get incomplete tasks
-    function getAvailableTasks() public view returns (TaskSummary[] memory) {
+    function getAvailableTasks()
+        public
+        view
+        returns (
+            uint[] memory,
+            string[] memory,
+            bool[] memory,
+            address[] memory
+        )
+    {
         uint count = 0;
 
         // Count the number of incomplete tasks
         for (uint i = 0; i < taskCounter; i++) {
-            if (!tasks[i].isActive) {
+            if (tasks[i].isActive) {
                 count++;
             }
         }
+        uint[] memory ids = new uint[](count);
+        string[] memory descriptions = new string[](count);
+        bool[] memory isActiveFlags = new bool[](count);
+        address[] memory assignedTos = new address[](count);
 
-        // Create a temporary array to hold TaskSummary
-        TaskSummary[] memory availableTasks = new TaskSummary[](count);
         uint index = 0;
 
         for (uint i = 0; i < taskCounter; i++) {
-            if (!tasks[i].isActive) {
-                availableTasks[index] = TaskSummary({
-                    id: tasks[i].id,
-                    description: "assigned",
-                    isActive: tasks[i].isActive,
-                    assignedTo: tasks[i].creator
-                });
+            if (tasks[i].isActive) {
+                ids[index] = tasks[i].id;
+                descriptions[index] = "assigned";
+                isActiveFlags[index] = tasks[i].isActive;
+                assignedTos[index] = tasks[i].creator;
                 index++;
             }
         }
 
-        return availableTasks;
+        return (ids, descriptions, isActiveFlags, assignedTos);
     }
     // Get task details by ID
     // function getTaskById(uint taskId) public view returns (Task memory) {
